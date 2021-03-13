c339b4fc6e90c955de9fb3c6c29b7393206ce843
Cimpean Cristi <ciolo_18@yahoo.com>
2021-03-13 20:14:17 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Expert contabil judiciar - Practician în insolvență</p>
+      <p>economist <strong>Răducea Marius-Adrian</strong></p>
+      <p>Alba-Iulia, str. Cloșca nr. 12 bl. CF 11, ap. 9</p>
+      <p><strong>Telefon mobil:</strong> 0744 997903</p>
+      <p><strong>Telefon fix:</strong> 0258 834108</p>
+      <p><strong>Fax:</strong> 0258 834108</p>
+      <p><strong>E-mail:</strong> <a href="mailto:m.raducea@yahoo.com">m.raducea@yahoo.com</a> </p>
+    </div>
+  );
+}
+
+export default App;
